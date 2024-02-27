c5e9783215de1e8f2494e5db1fba64e104634561
RenannLimaa <renanrod2403@gmail.com>
2024-02-26 22:17:36 -0300

    add functionality that shows only questions that are published

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,7 @@
 from django.db.models import F
-from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 from .models import Choice, Question
 # Create your views here.
@@ -12,7 +12,7 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 
 class DetailView(generic.DetailView):
