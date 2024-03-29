70fcd0a39af257dc02ea5ff85b273d2575999b74
RenannLimaa <renanrod2403@gmail.com>
2024-03-29 13:32:21 -0300

    render page with list of tasks

--- todo_project/todo_app/views.py
+++ todo_project/todo_app/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
 # Create your views here.
+
+
+def my_tasks(request):
+    tasks = Task.objects.all()
+    return render(request, "todo_app/mytasks.html", {"tasks": tasks})
