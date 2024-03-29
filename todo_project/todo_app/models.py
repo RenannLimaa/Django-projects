12e885e05a5d9181f2b0fe69ec27b2460ee95936
RenannLimaa <renanrod2403@gmail.com>
2024-03-29 13:26:19 -0300

    fix typo in inheritance

--- todo_project/todo_app/models.py
+++ todo_project/todo_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Task(models.Models):
+class Task(models.Model):
     task_name = models.CharField(max_length=64)
     description = models.CharField(max_length=256)
     creation_date = models.DateTimeField(auto_now_add=True)
