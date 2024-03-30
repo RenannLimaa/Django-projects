c51efcfcfbca8c70220d74534c6de39ccf7cdf55
RenannLimaa <renanrod2403@gmail.com>
2024-03-30 13:41:03 -0300

    make the description and due_date nullable

--- todo_project/todo_app/models.py
+++ todo_project/todo_app/models.py
@@ -3,7 +3,10 @@ from django.db import models
 
 class Task(models.Model):
     task_name = models.CharField(max_length=64)
-    description = models.CharField(max_length=256)
+    description = models.CharField(max_length=256, blank=True, null=True)
     creation_date = models.DateTimeField(auto_now_add=True)
-    due_date = models.DateTimeField()
+    due_date = models.DateTimeField(blank=True, null=True)
     is_done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.task_name
