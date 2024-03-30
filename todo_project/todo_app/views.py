db00e74c725f64d3d4ddba877c308860b9f218f2
RenannLimaa <renanrod2403@gmail.com>
2024-03-30 13:45:43 -0300

    add add_task view

--- todo_project/todo_app/views.py
+++ todo_project/todo_app/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render, redirect
 from .models import Task
-from .forms import TaskForm
 # Create your views here.
 
 
 def my_tasks(request):
     tasks = Task.objects.all()
     return render(request, "todo_app/mytasks.html", {"tasks": tasks})
+
+
+def add_task(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        due_date = request.POST.get("due-date")
+        description = request.POST.get("description")
+
+        if due_date == "":
+            due_date = None
+
+        elif description == "":
+            description = None
+
+        task = Task(task_name=title, description=description, due_date=due_date)
+        task.save()
+
+        return redirect("my_tasks")
+    else:
+        return render(request, "todo_app/mytasks.html")
