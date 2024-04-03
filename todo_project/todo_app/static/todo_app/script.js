191910c330f91f4e5a34716d4e93675b92267882
RenannLimaa <renanrod2403@gmail.com>
2024-04-03 13:08:06 -0300

    rename variables and add formatDate function

--- todo_project/todo_app/static/todo_app/script.js
+++ todo_project/todo_app/static/todo_app/script.js
@@ -1,12 +1,55 @@
-addTaskButton = document.querySelector(".add-task-btn");
-closeModalButton = document.querySelector(".close-modal-btn");
-modal = document.querySelector(".add-task-modal");
+const addTaskButton = document.querySelector(".add-task-btn");
+const closeModalButtons = document.querySelectorAll(".close-modal-btn");
+const addTaskModal = document.querySelector(".add-task-modal");
+const modals = document.querySelectorAll(".modal");
+
+const taskButtons = document.querySelectorAll(".task-list");
+const editTaskModal = document.querySelector(".edit-task-modal");
+const editTaskForm = document.querySelector("#edit-task-form");
 
 addTaskButton.onclick = function () {
-    modal.showModal();
+        addTaskModal.showModal();
 }
 
+closeModalButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        const modalToClose = button.closest('.modal');
+        if (modalToClose) {
+            modalToClose.close();
+        }
+    });
+});
+function formatDate(inputDate) {
+    var split_string = inputDate.split(','); 
+    var datePart = split_string[0];
+    var year = split_string[1]; 
 
-closeModalButton.onclick = function () {
-    modal.close();
+    var date = [datePart, year].join(",");
+    var date = new Date(date); 
+
+        
+    var month = (date.getMonth() + 1).toString().padStart(2, '0');
+    var day = date.getDate().toString().padStart(2, '0');
+    var year = date.getFullYear();
+
+    return year + '-' + month + '-' + day 
 }
+
+taskButtons.forEach(taskButton => {
+    taskButton.addEventListener('click', function () {
+        const titleInput = editTaskForm.querySelector("#title");
+        const descriptionInput = editTaskForm.querySelector("#description");
+        const dueDateInput = editTaskForm.querySelector("#due-date");
+
+
+        const taskName = taskButton.dataset.taskName;
+        const taskDescription = taskButton.dataset.taskDescription;
+        const taskDueDate = taskButton.dataset.taskDueDate;
+
+        titleInput.value = taskName;
+        descriptionInput.value = taskDescription;
+        dueDateInput.value = formatDate(taskDueDate);
+
+        editTaskModal.showModal();
+    });
+});
