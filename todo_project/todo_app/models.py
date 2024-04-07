13a3ed6da0328ba548bc9365ac459830be6cdf02
RenannLimaa <renanrod2403@gmail.com>
2024-04-07 11:24:44 -0300

    set primary key to Task model

--- todo_project/todo_app/models.py
+++ todo_project/todo_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Task(models.Model):
+    task_id = models.BigAutoField(primary_key=True)
     task_name = models.CharField(max_length=64)
     description = models.CharField(max_length=256, blank=True, null=True)
     creation_date = models.DateTimeField(auto_now_add=True)
