81edc64b7e08c5ffb37f0102dd056a17828034d4
RenannLimaa <renanrod2403@gmail.com>
2024-04-20 22:37:49 -0300

    fetch weather data and render index template

--- weather_project/weather_app/views.py
+++ weather_project/weather_app/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+import requests
 
-# Create your views here.
+
+def index(request):
+    API_KEY = open("/home/renan/programming/Django/weather_project/API_KEY", "r").read().strip()
+    current_weather_url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID={}"
+
+    if request.method == "POST":
+        city = request.POST.get("city")
+
+        weather_data = fetch_weather(city, API_KEY, current_weather_url)
+
+        context = {
+            "weather_data": weather_data
+        }
+
+        return render(request, "weather_app/index.html", context)
+    else:
+        return render(request, "weather_app/index.html")
+
+
+def fetch_weather(city, api_key, current_weather_url):
+    response = requests.get(current_weather_url.format(city, api_key)).json()
+
+    print(response)
+
+    weather_data = {
+        "city": city,
+        "temperature": round(response['main']['temp'], 2),
+        "description": response['weather'][0]['description'],
+        "icon": response['weather'][0]['icon']
+    }
+
+    return weather_data
